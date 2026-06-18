6623681c99250d88bfcbdc86eb1397ce17ef41b2
azad271 <azadmiah115271@gmail.com>
2026-06-18 17:02:16 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -16,4 +16,37 @@ interface Clickable {
             myButton.onClick();
         }
     }
+
+
+
+
+
+
+//Aanother Class
+
+
+class Countdown {
+    
+    void start() {
+        
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+        
+        
+        System.out.println("Go!");
+    }
+}
+
+public class main33 {
+    public static void main(String[] args) {
+        
+        Countdown countdown = new Countdown();
+        
+        
+        countdown.start();
+    }
+}
+
+
     
